5bea7efe1b7a1b01c1ca9761b233a76f0ca6cd3c
GSNSAMPATH <novrithtechnolgy@gmail.com>
2026-03-12 15:49:55 +0530

    update villa finel without meta tags

--- src/app/available-villas/page.tsx
+++ src/app/available-villas/page.tsx
@@ -1,15 +1,29 @@
+import { Suspense } from "react";
+import { connection } from "next/server";
 import AvailableVillasClient from "../components/Available-villas/AvailableVillasClient";
 import AvailableVillasHero from "../components/Available-villas/Heropage";
 import Navbar from "../components/Navbar";
 
-
-export default function AvailableVillasPage() {
-  return(
-  <>
-  <Navbar />
-  <AvailableVillasHero/>
-  <AvailableVillasClient />
-  </>
-  )
+function AvailableVillasFallback() {
+  return (
+    <section className="min-h-screen pb-20">
+      <div className="text-center text-gray-500 py-20">
+        Loading available villas...
+      </div>
+    </section>
+  );
 }
-   
+
+export default async function AvailableVillasPage() {
+  await connection();
+
+  return (
+    <>
+      <Navbar />
+      <AvailableVillasHero />
+      <Suspense fallback={<AvailableVillasFallback />}>
+        <AvailableVillasClient />
+      </Suspense>
+    </>
+  );
+}
