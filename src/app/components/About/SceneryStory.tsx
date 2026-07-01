dc67cf7d2810d86cc858b3f411dafc496e70d126
GSNSAMPATH <novrithtechnolgy@gmail.com>
2026-07-01 12:41:11 +0530

    update mobile

--- src/app/components/About/SceneryStory.tsx
+++ src/app/components/About/SceneryStory.tsx
@@ -24,7 +24,7 @@ export default function SceneryStory() {
           <br/> 
             A European family he guided fell in love with Sri Lanka and chose to build a villa here. That first project became Villa Mandalay, a peaceful hilltop retreat that marked the beginning of something greater.
         </p>
-          <div className="relative w-full md:w-7/3 h-[300px] md:h-[480px] rounded-[20px]">
+          <div className="hidden md:block relative w-full md:w-7/3 h-[300px] md:h-[480px] rounded-[20px]">
           <Image
             src="https://res.cloudinary.com/dpjmcup95/image/upload/v1772896456/IMG-20250927-WA0037_1_p0gqeo.jpg"
             alt="Scenery Story"
@@ -41,6 +41,15 @@ export default function SceneryStory() {
 Treetop Resort followed a different path. Once a well-known banquet and wedding venue in the area, it was reimagined into a spacious private retreat, now welcoming large groups and gatherings in a setting designed for connection.
 Today, Scenery Villas is a curated collection of unique homes, each chosen not just for its location, but for the feeling it offers. Together, they create a space where guests can slow down, reconnect, and experience the true warmth of Sri Lanka.
         </p>
+
+        <div className="md:hidden relative w-full md:w-7/3 h-[300px] md:h-[480px] rounded-[20px]">
+          <Image
+            src="https://res.cloudinary.com/dpjmcup95/image/upload/v1772896456/IMG-20250927-WA0037_1_p0gqeo.jpg"
+            alt="Scenery Story"
+            fill
+            className="mt-4 md:mt-8 object-cover rounded-[20px] border border-gray-200"
+          />
+          </div>
 
       </div>
      </Container>
